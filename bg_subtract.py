e026261581d45c330c6cc1d9578f6d1fe41f2e78
karnnumart <k.iamthammarak@hotmail.com>
2019-10-08 22:21:15 +0700

    print progress
    show in both % and /

--- bg_subtract.py
+++ bg_subtract.py
@@ -32,6 +32,13 @@ max_speed_ratio = 0.2
 
 ### -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  - ###
 img_num = 0
+cap = cv2.VideoCapture('video/'+video_name+'.mp4')
+model = InceptionResNetV2(weights='imagenet')
+MOG2 = cv2.createBackgroundSubtractorMOG2(varThreshold=trsh,history=hist,detectShadows=shadow)
+number = 0
+p_list = []
+print("Process Started",video_name)
+
 class Person:
     def __init__(self, x, y, w, h, n):
         self.x = int(x)
@@ -64,6 +71,7 @@ class Person:
     def saveImg(self, frame, x, y, w, h):
         if not self.isSaved and self.direction == -1: # is up
             global img_num
+            global cap
             y0 = y-extra_top if y-extra_top > 0 else 0
             img = cv2.resize(frame[y0:y+h, x:x+w], (299,299))
             img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -81,19 +89,13 @@ class Person:
                     cv2.imwrite("crop_img/"+video_name+"/bike_"+str(img_num)+".jpg",frame[y0:y+h, x:x+w])
                     found = True
             if found:
-                print()
-                print('bike #'+str(img_num),'-',t3)
+                total = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+                current = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
+                print('Progress:', "{0:.3f}".format(100*current/total)+'%('+str(current)+'/'+str(total)+')', 'bike #'+str(img_num),'-',t3)
                 img_num += 1
             self.isSaved = True
 
-model = InceptionResNetV2(weights='imagenet')
-cap = cv2.VideoCapture('video/'+video_name+'.mp4')
-MOG2 = cv2.createBackgroundSubtractorMOG2(varThreshold=trsh,history=hist,detectShadows=shadow)
-number = 0
-p_list = []
-print("Process Started",video_name)
 while True:
-    print(">",end="")
     _, frame = cap.read()
     half_frame = frame.copy()
     half_frame = half_frame[int(half_frame.shape[0]*disc_ratio):,:]
