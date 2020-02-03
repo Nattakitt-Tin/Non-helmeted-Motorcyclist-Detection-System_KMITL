c66424da366ef086f35c11e8ea92e73f0d79b4a6
karnnumart <k.iamthammarak@hotmail.com>
2020-02-03 21:36:30 +0700

    Mirror, Colorful and The line
    the line can change in real time (show = True)
    Mirror and prediction again if result of first prediction is not helmet. (Because training use only rightward image)
    Edit cropping a bit, crop on first frame only.
    Colorful border for each object, we can easily see how each object blink around like crazy hopper.

--- Hel(p)Mate.py
+++ Hel(p)Mate.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
+import random
 
 from tensorflow.keras.models import load_model
 from tensorflow.keras.applications.inception_resnet_v2 import InceptionResNetV2 
@@ -12,10 +13,10 @@ from tensorflow.keras.applications.inception_resnet_v2 import preprocess_input, 
 current_video = 'xxx'
 print('load model')
 general_model = InceptionResNetV2(weights='imagenet')
-print('InceptionResnetV2 loaded complete')
+print('Custom model loaded complete')
 helmet_model = load_model("incV3_final.h5")
 MOG2 = cv2.createBackgroundSubtractorMOG2(varThreshold=16,history=500,detectShadows=True)
-print('helmet model loaded complete')
+print('Helmet model loaded complete')
 
 img_num = 0
 extra_top = 10
@@ -27,29 +28,44 @@ crop = False
 scale = 0.7
 
 class Person:
-    def __init__(self, x, y, w, h, n):
-        self.x = int(x)
-        self.y = int(y)
+    def __init__(self, x, y, w, h, n, life):
+        self.xc = int(x+(w/2))
+        self.yc = int(y+(h/2))
+        self.x = x
+        self.y = y
         self.w = w
         self.h = h
-        self.first_y = int(y)
+        self.min_y = int(y)
+        self.first_y = self.yc
         self.n = n
-        self.life = 5
+        self.life = int(life)
         
+        self.R = random.randint(0,255)
+        self.G = random.randint(0,255)
+        self.B = random.randint(0,255)
         self.isSaved = False
         self.direction = 0
 
-    def update(self, xc, yc, frame, x, y, w, h, bottom):
-        if yc - self.first_y < 0:
+    def update(self, frame, x, y, w, h, bottom,the_line):
+        if self.yc - self.first_y < 0:
             self.direction = -1 #up
         else:
             self.direction = 1 # down
         self.life = 5
-        self.x = int(xc)
-        self.y = int(yc)
+        if self.direction < 0:
+            if y > self.min_y:
+                y = self.min_y
+            else:
+                self.min_y = y
+        
+        self.x = int(x)
+        self.y = int(y)
         self.w = w
         self.h = h
-        if bottom - 50 < y+h < bottom: #object float off bottom (default should be 0 without black bar)
+        self.xc = int(x+(w/2))
+        self.yc = int(y+(h/2))
+        
+        if the_line < y+h < bottom: #object float off bottom
             self.saveImg(frame, x, y, w, h)
         
     def saveImg(self, frame, x, y, w, h):
@@ -78,10 +94,14 @@ class Person:
                     img = img/255.
                     preds = helmet_model.predict([[img]])
                     helmet = preds[0][1]
-                    result_path = "extracted/helmet/bike_"+str(img_num)+"_"+str(current_video)+".jpg"
-                    if helmet < 0.5:
-                        # print('+ + + no helmet! + + +',helmet)
-                        result_path = "extracted/no_helmet/bike_"+str(img_num)+"_"+str(current_video)+".jpg"
+
+                    result_path = "extracted/no_helmet/bike_"+str(img_num)+"_"+str(current_video)+".jpg"
+                    if helmet > 0.5: #helmet
+                        img_flip = cv2.flip(img, 1)
+                        preds_flip = helmet_model.predict([[img_flip]])
+                        helmet_flip = preds_flip[0][1]
+                        if helmet_flip > 0.5: #helmet
+                            result_path = "extracted/helmet/bike_"+str(img_num)+"_"+str(current_video)+".jpg"
                     cv2.imwrite(result_path,bike_img)
                     found = True
             if found:
@@ -92,63 +112,76 @@ class Person:
 
 def mouse_drawing(event, x, y, flags, params):
     global x1,y1,x2,y2,mouse_down,crop
-    if not crop:
-        if event == cv2.EVENT_LBUTTONDOWN:
-            x1 = int(x/scale)
-            y1 = int(y/scale)
-            mouse_down = True
-        elif event == cv2.EVENT_LBUTTONUP:
-            mouse_down = False
-            if x1 > x2:
-                x1,x2 = x2,x1
-            if y1 > y2:
-                y1,y2 = y2,y1
-        if mouse_down:
-            x2 = int(x/scale)
-            y2 = int(y/scale)
-    else:
-        if event == cv2.EVENT_RBUTTONUP and not mouse_down:
-            crop = False
-            x1,y1,x2,y2 = 0,0,0,0
+    if event == cv2.EVENT_LBUTTONDOWN:
+        x1 = int(x/scale)
+        y1 = int(y/scale)
+        mouse_down = True
+    elif event == cv2.EVENT_LBUTTONUP:
+        mouse_down = False
+        if x1 > x2:
+            x1,x2 = x2,x1
+        if y1 > y2:
+            y1,y2 = y2,y1
+    if mouse_down:
+        x2 = int(x/scale)
+        y2 = int(y/scale)
 
-def main(video_name_list, bike_h, max_speed_ratio, show=True):
+def main(video_name_list, bike_h, max_speed_ratio, show=True, extension='.mp4', real_fps=False):
     global current_video, progress, crop
+    
     cv2.namedWindow("BGR")
     cv2.setMouseCallback("BGR", mouse_drawing)
     for video_name in video_name_list:
         current_video = video_name
-        cap = cv2.VideoCapture('video/'+video_name+'.mp4')
+        cap = cv2.VideoCapture('video/'+video_name+extension)
         obj_number = 0
         progress = 0
         p_list = []
-        print("Process Started on Video:",video_name+'.mp4')
+        print("Process Started on Video:",video_name+extension)
         _, first = cap.read()
         if first is None:
                 print('Nothing to read, Closing Process')
                 continue
         while np.average(first) < 10: #skip black frame
             _, first = cap.read()
-        
+        left,right,top,bottom = 0,0,0,0
         while True:
-            if not mouse_down:
-                _, frame = cap.read()
+            disp_frame = first.copy()
+            if mouse_down:
+                cv2.rectangle(disp_frame,(x1,y1),(x2,y2),(0, 0, 252), 2)
+            width = int(disp_frame.shape[1]*scale)
+            height = int(disp_frame.shape[0]*scale)
+            cv2.imshow('BGR', cv2.resize(disp_frame, (width, height)))
+            key = cv2.waitKey(1)
+            if (x1 > 0 or x2 > 0 or  y1 > 0 or y2 > 0) and not mouse_down:
+                left,right,top,bottom = x1,x2,y1,y2
+                crop = True
+                # cv2.destroyAllWindows()
+                break
+        fps = None
+        delay = None
+        the_line = bottom-top - 100
+        while True:
+            _, frame = cap.read()
             if frame is None:
                 print('Video Ended, Closing Process')
                 break
+            if fps is None:
+                fps = cap.get(cv2.CAP_PROP_FPS)
+                print('fps:',fps)
+                delay = int(1000/fps) if real_fps else 1
             total = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
             current = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
             sys.stdout.write("Progress: "+ "{0:.3f}".format(100*current/total)+'% ('+str((current//30)//60)+':'+str((current//30)%60)+'/'+str((total//30)//60)+':'+str((total//30)%60)+')')
             sys.stdout.flush()
             sys.stdout.write("\b" * (100))
-
-            if (x1 > 0 or x2 > 0 or  y1 > 0 or x2 > 0) and not mouse_down:
-                frame = frame[y1:y2, x1:x2]
-                crop = True
+            # print('the line is', the_line)
+            
+            frame = frame[top:bottom, left:right]
             frame_copy = frame.copy()
             disp_frame = frame.copy()
             if mouse_down:
-                cv2.rectangle(disp_frame,(x1,y1),(x2,y2),(6, 186, 252), 2)
-
+                the_line = y2
             binary = MOG2.apply(frame_copy)
             _, binary = cv2.threshold(binary,200,255,cv2.THRESH_BINARY)
             kernel = np.ones((5,5),np.uint8)
@@ -160,42 +193,50 @@ def main(video_name_list, bike_h, max_speed_ratio, show=True):
             ctrs, hier = cv2.findContours(binary, cv2.RETR_EXTERNAL , cv2.CHAIN_APPROX_SIMPLE)
             for i, ctr in enumerate(ctrs):
                 x, y, w, h = cv2.boundingRect(ctr)  # get xywh from points
-                if h > bike_h:
+                if h > bike_h and h > w:
                     xc, yc = x+(w/2), y+(h/2)
                     exist = False
                     for p in p_list:
-                        accepted_diff =  int(frame_copy.shape[0]*max_speed_ratio)
-                        if -accepted_diff < xc-p.x < accepted_diff and -accepted_diff < yc-p.y < accepted_diff: # center around some object in previous frame, check if its same object
-                            p.update(xc, yc, frame_copy, x, y, w, h, frame.shape[0])
+                        accepted_diff =  int(bike_h*max_speed_ratio)
+                        if -accepted_diff < xc-p.xc < accepted_diff and -accepted_diff < yc-p.yc < accepted_diff: # center around some object in previous frame, check if its same object
+                            p.update(frame_copy, x, y, w, h, frame.shape[0], the_line)
                             exist = True
                             break
                     if not exist:
-                        p_list.append(Person(xc, yc, w, h, obj_number)) # create new person
+                        p_list.append(Person(x, y, w, h, obj_number, fps)) # create new person
                         obj_number+=1
                     if show:
-                        cv2.rectangle(disp_frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                        for p in p_list:
+                            cv2.rectangle(disp_frame, (p.x, p.y), (p.x + p.w, p.y + p.h), (p.B, p.G, p.R), 2)
             for p in p_list.copy():
                 p.life -= 1
                 if p.life == 0:
                     p_list.remove(p)
                 p.updated = False
                 if show:
-                    cv2.putText(disp_frame,str(p.n),(p.x,p.y),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
+                    cv2.putText(disp_frame,str(p.n),(p.xc,p.yc),cv2.FONT_HERSHEY_SIMPLEX,1,(0,255,0),2)
             # cv2.drawContours(frame, ctrs, -1, (0, 0, 255), 1)
             if show:
                 width = int(disp_frame.shape[1]*scale)
                 height = int(disp_frame.shape[0]*scale)
+                cv2.line(disp_frame, (0,the_line), (right,the_line), (69,255,36), 2)
                 cv2.imshow('BGR', cv2.resize(disp_frame, (width, height)))
                 cv2.imshow('Binary', cv2.resize(binary, (width, height)))
-                key = cv2.waitKey(1) ################################# DELAY IS HERE ####################################
+                key = cv2.waitKey(delay) ################################# DELAY IS HERE ####################################
                 if key == 32:
                     # print("space")
                     key = cv2.waitKey(0)
+                if key == 44: # <
+                    cap.set(cv2.CAP_PROP_POS_FRAMES, cap.get(cv2.CAP_PROP_POS_FRAMES)-100)
+                if key == 46: # >
+                    cap.set(cv2.CAP_PROP_POS_FRAMES, cap.get(cv2.CAP_PROP_POS_FRAMES)+100)
                 if key == 27:
                     # print("esc")
                     break
+            
+
         cap.release()
         cv2.destroyAllWindows()
 
 
-main(video_name_list=['b2'], bike_h=200, max_speed_ratio=0.2)
+main(['d1'], bike_h=100, max_speed_ratio=2, extension='.avi', real_fps=False, show=True)
