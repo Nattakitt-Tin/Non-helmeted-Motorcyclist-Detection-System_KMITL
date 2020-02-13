48a8d4d694f75a51e5be06f03e33c2c1a778ff6d
karnnumart <k.iamthammarak@hotmail.com>
2020-02-13 10:29:29 +0700

    update

--- python_section/testkeras.py
+++ python_section/testkeras.py
@@ -32,3 +32,116 @@ for i in range(60):
 print('scooter found',count)
 
 # Predicted: [(u'n02504013', u'Indian_elephant', 0.82658225), (u'n01871265', u'tusker', 0.1122357), (u'n02504458', u'African_elephant', 0.061040461)]
+
+#Hello test, this is update
+#why would you need to keep update when all your task is done.
+#stupid graph checker make no sense
+#but here's the update 
+
+
+# Define the upper and lower boundaries for a color to be considered "Blue"
+blueLower = np.array([100, 60, 60])
+blueUpper = np.array([140, 255, 255])
+
+# Define a 5x5 kernel for erosion and dilation
+kernel = np.ones((5, 5), np.uint8)
+
+# Initialize deques to store different colors in different arrays
+bpoints = [deque(maxlen=512)]
+gpoints = [deque(maxlen=512)]
+rpoints = [deque(maxlen=512)]
+ypoints = [deque(maxlen=512)]
+
+# Initialize an index variable for each of the colors 
+bindex = 0
+gindex = 0
+rindex = 0
+yindex = 0
+
+# Just a handy array and an index variable to get the color-of-interest on the go
+# Blue, Green, Red, Yellow respectively
+colors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (0, 255, 255)] 
+colorIndex = 0
+
+
+# Create a blank white image
+paintWindow = np.zeros((471,636,3)) + 255
+
+# Draw buttons like colored rectangles on the white image
+paintWindow = cv2.rectangle(paintWindow, (40,1), (140,65), (0,0,0), 2)
+paintWindow = cv2.rectangle(paintWindow, (160,1), (255,65), colors[0], -1)
+paintWindow = cv2.rectangle(paintWindow, (275,1), (370,65), colors[1], -1)
+paintWindow = cv2.rectangle(paintWindow, (390,1), (485,65), colors[2], -1)
+paintWindow = cv2.rectangle(paintWindow, (505,1), (600,65), colors[3], -1)
+
+# Label the rectanglular boxes drawn on the image
+cv2.putText(paintWindow, "CLEAR ALL", (49, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 2, cv2.LINE_AA)
+cv2.putText(paintWindow, "BLUE", (185, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+cv2.putText(paintWindow, "GREEN", (298, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+cv2.putText(paintWindow, "RED", (420, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+cv2.putText(paintWindow, "YELLOW", (520, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (150,150,150), 2, cv2.LINE_AA)
+
+# Create a window to display the above image (later)
+cv2.namedWindow('Paint', cv2.WINDOW_AUTOSIZE)
+
+# Load the video
+camera = cv2.VideoCapture(0)
+
+# Keep looping
+while True:
+    # Grab the current paintWindow
+    (grabbed, frame) = camera.read()
+    frame = cv2.flip(frame, 1)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # Check to see if we have reached the end of the video (useful when input is a video file not a live video stream)
+    if not grabbed:
+        break    
+    
+    # Add the same paint interface to the camera feed captured through the webcam (for ease of usage)
+    frame = cv2.rectangle(frame, (40,1), (140,65), (122,122,122), -1)
+    frame = cv2.rectangle(frame, (160,1), (255,65), colors[0], -1)
+    frame = cv2.rectangle(frame, (275,1), (370,65), colors[1], -1)
+    frame = cv2.rectangle(frame, (390,1), (485,65), colors[2], -1)
+    frame = cv2.rectangle(frame, (505,1), (600,65), colors[3], -1)
+    cv2.putText(frame, "CLEAR ALL", (49, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+    cv2.putText(frame, "BLUE", (185, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+    cv2.putText(frame, "GREEN", (298, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+    cv2.putText(frame, "RED", (420, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA)
+    cv2.putText(frame, "YELLOW", (520, 33), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (150,150,150), 2, cv2.LINE_AA)
+
+    # Determine which pixels fall within the blue boundaries and then blur the binary image
+    blueMask = cv2.inRange(hsv, blueLower, blueUpper)
+    blueMask = cv2.erode(blueMask, kernel, iterations=2)
+    blueMask = cv2.morphologyEx(blueMask, cv2.MORPH_OPEN, kernel)
+    blueMask = cv2.dilate(blueMask, kernel, iterations=1)
+
+    # Find contours in the image
+    (_, cnts, _) = cv2.findContours(blueMask.copy(), cv2.RETR_EXTERNAL,
+    	cv2.CHAIN_APPROX_SIMPLE)
+
+
+    # Check to see if any contours (blue stuff) were found
+    if len(cnts) > 0:
+    	# Sort the contours and find the largest one -- we assume this contour correspondes to the area of the bottle cap
+        cnt = sorted(cnts, key = cv2.contourArea, reverse = True)[0]
+        # Get the radius of the enclosing circle around the found contour
+        ((x, y), radius) = cv2.minEnclosingCircle(cnt)
+        # Draw the circle around the contour
+        cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+        # Get the moments to calculate the center of the contour (in this case a circle)
+        M = cv2.moments(cnt)
+        center = (int(M['m10'] / M['m00']), int(M['m01'] / M['m00']))
+
+    
+    # Check to see if any contours (blue stuff) were found
+    if len(cnts) > 0:
+    	# Sort the contours and find the largest one -- we assume this contour correspondes to the area of the bottle cap
+        cnt = sorted(cnts, key = cv2.contourArea, reverse = True)[0]
+        # Get the radius of the enclosing circle around the found contour
+        ((x, y), radius) = cv2.minEnclosingCircle(cnt)
+        # Draw the circle around the contour
+        cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+        # Get the moments to calculate the center of the contour (in this case a circle)
+        M = cv2.moments(cnt)
+        center = (int(M['m10'] / M['m00']), int(M['m01'] / M['m00']))
